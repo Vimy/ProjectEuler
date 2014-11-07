b4e9570bd5878d1efdcbd2a473d6b52e0796be6e
MatthiasV <matthias.vermeulen@gmail.com>
2014-11-07 21:57:06 +0100

    Initial Commit

---
+++ ProjectEuler/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProjectEuler
+//
+//  Created by Matthias Vermeulen on 7/11/14.
+//  Copyright (c) 2014 Noizy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
